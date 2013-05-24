de15492ef5626567b800642b7647971e42849ccd
Mike C. Fletcher <mcfletch@vrplumber.com>
2013-05-24 01:02:57 -0400

    Add a setting ZINNIA_TITLE which, if present is used as the title for Open Search

--- zinnia/context_processors.py
+++ zinnia/context_processors.py
@@ -1,9 +1,13 @@
 """Context Processors for Zinnia"""
 from zinnia import __version__
+from zinnia import settings
 
 
 def version(request):
     """
     Add version of Zinnia to the context.
     """
-    return {'ZINNIA_VERSION': __version__}
+    return {
+        'ZINNIA_VERSION': __version__,
+        'ZINNIA_TITLE': settings.TITLE,
+    }
